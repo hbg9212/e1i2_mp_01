c6612f95ff3a42d3510cd04e400132216eb290e8
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-09 19:48:23 +0900

    2023.08.09 판넬 기능 추가 완료

---
+++ mp_01/Assets/Scripts/panel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class panel : MonoBehaviour
+{
+    public Text bestScore;
+    public Text nowScore;
+
+    void Start()
+    {
+
+
+    }
+
+    public void bestScoreSet(string name)
+    {
+        bestScore.text = name;
+    }
+    public void nowScoreSet(string name)
+    {
+        nowScore.text = name;
+    }
+
+    public void retryGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
